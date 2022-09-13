d76442879b166a4f8d333155bb831c95e242089c
Jack123-Wang <62133474+Jack123-Wang@users.noreply.github.com>
2022-09-13 15:39:36 +0800

    Update MAGeCK.sh

--- MAGeCK/MAGeCK.sh
+++ MAGeCK/MAGeCK.sh
@@ -1,3 +1,3 @@
-mageck count -l 'Brie library(mageck)+non-target.csv' -n demo --sample-label lib,input1,input2,outpu1,output2  --fastq hko-Brie.top20.fasta.gz Input1.top20.fasta.gz  Input2.top20.fasta.gz Output1.top20.fasta.gz Output2.top20.fasta.gz 
+mageck count -l 'Brie library(mageck)+non-target.csv' -n demo --sample-label lib,input1,input2,outpu1,output2  --fastq Brie.top20.fasta.gz Input1.top20.fasta.gz  Input2.top20.fasta.gz Output1.top20.fasta.gz Output2.top20.fasta.gz 
 
 mageck test -k demo.count.txt -c input1,input2 -t outpu1,output2 -n demo
